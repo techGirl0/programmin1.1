f2c78f36b84da9b35efe429e75752d26e4e289d9
Sediqa Talibi <254766232+techGirl0@users.noreply.github.com>
2026-01-20 18:28:25 +0100

    Add solution for matrix transpose in Java

---
+++ matrixTranspose.java
@@ -0,0 +1,29 @@
+public class TransposeMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Transposed Matrix:");
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[i].length; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
