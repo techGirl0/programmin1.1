90e5959238ca19aeb09f0ab6e812b24917a88019
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 10:04:55 +0100

    Add program file set up

---
+++ week4_data_structure/FinancialTransactionTracker.java
@@ -0,0 +1,9 @@
+package week4_data_structure;
+import java.util.ArrayList; 
+
+public class FinancialTransactionTracker {
+    public static void main(String[] args) {
+        
+        ArrayList<String> history = new ArrayList<>(); 
+    }
+}
