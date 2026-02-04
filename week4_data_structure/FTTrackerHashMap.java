4975c95f65f2a473e039e03d0f427bbe6b7b0537
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 11:22:51 +0100

    Add programm skleton

---
+++ week4_data_structure/FTTrackerHashMap.java
@@ -0,0 +1,13 @@
+package week4_data_structure;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class FTTrackerHashMap{
+
+    public static void main(String[] args){
+       
+
+    }
+}
