e8475a00d6f4a586894fb8fae04ca76036823499
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 11:51:51 +0100

    Implement Add Transaction using HashMap with unique ID

--- week4_data_structure/FTTrackerHashMap.java
+++ week4_data_structure/FTTrackerHashMap.java
@@ -1,11 +1,13 @@
 package week4_data_structure;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.time.LocalDate;
 
-public class FinancialTransactionTracker {
+public class FTTrackerHashMap {
     public static void main(String[] args) {
         HashMap<Integer, String> transactions = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
+        int choice; 
 
         while (true) {
             System.out.println("\nFinancial Transaction Tracker");
@@ -16,8 +18,32 @@ public class FinancialTransactionTracker {
             System.out.println("5. Print all");
             System.out.print("Enter your choice: ");
 
-            int choice = scanner.nextInt();
-            scanner.nextLine(); 
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter type (Purchase/Sale): ");
+                    String type = scanner.nextLine();
+                    System.out.print("Enter amount: ");
+                    double amount = scanner.nextDouble();
+                    scanner.nextLine(); 
+                    LocalDate date = LocalDate.now();
+
+                    int id = transactions.size() + 1;
+                    transactions.put(id, type + "," + amount + "," + date);
+
+                    System.out.println("Transaction added with ID: " + id);
+                    break;
+
+                case 4:
+                    System.out.println("Exiting tracker. Goodbye!");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Option selected: " + choice);
+            }
         }
     }
 }
