ca0610325af29c8fa6ed868e4642f57db0482ccf
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 09:47:27 +0100

    Exercise: 2D array row and column counts

---
+++ week4_data_structure/Arrays.java
@@ -0,0 +1,11 @@
+package week4_data_structure;
+
+public class Arrays {
+    public static void main(String[] args) {
+      int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
+
+      System.out.println("Rows: " + myNumbers.length);            
+      System.out.println("Cols in row 0: " + myNumbers[0].length); 
+      System.out.println("Cols in row 1: " + myNumbers[1].length); 
+  }
+}
