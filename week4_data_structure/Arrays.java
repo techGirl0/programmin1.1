add15c317a55e92203ec750a9eaa2e1b14afb261
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 09:51:53 +0100

    Exercise: loop through 2D array and print elements

--- week4_data_structure/Arrays.java
+++ week4_data_structure/Arrays.java
@@ -2,10 +2,22 @@ package week4_data_structure;
 
 public class Arrays {
     public static void main(String[] args) {
+      // 2D arrays
       int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
 
       System.out.println("Rows: " + myNumbers.length);            
       System.out.println("Cols in row 0: " + myNumbers[0].length); 
       System.out.println("Cols in row 1: " + myNumbers[1].length); 
+
+      // Looing through 2D arrays
+
+      int[][] myNumbers2 = { {1, 4, 2}, {3, 6, 8, 5, 2} };
+
+      for (int row = 0; row < myNumbers2.length; row++) {
+        for (int col = 0; col < myNumbers[row].length; col++) {
+          System.out.println("myNumbers[" + row + "][" + col + "] = " + myNumbers2[row][col]);
+        }
+      }
+  
   }
 }
