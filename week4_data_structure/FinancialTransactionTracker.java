95064b22524ca8e51756c98d049dec6c083a22a6
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 11:02:00 +0100

    Fix: add if statmant to exit Infinite Loop

--- week4_data_structure/FinancialTransactionTracker.java
+++ week4_data_structure/FinancialTransactionTracker.java
@@ -1,13 +1,31 @@
-package week4_data_structure;
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FinancialTransactionTracker {
     public static void main(String[] args) {
-        
-        ArrayList<String> history = new ArrayList<>();
-        
+        ArrayList<String> transactions = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-}
+        while (true) {
+            System.out.println("\nFinancial Transaction Tracker");
+            System.out.println("1. Add Transaction");
+            System.out.println("2. Transactions Yesterday");
+            System.out.println("3. Total Income and Expense");
+            System.out.println("4. Exit");
+            System.out.println("5. Print all");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            if (choice == 4) {
+                System.out.println("Exiting tracker. Goodbye!");
+                break; 
+            }
+
+            System.out.println("You selected option: " + choice);
+        }
+
+        scanner.close();
+    }
 }
