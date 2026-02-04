ae33ed2b87ef58d7a5b96578037fd154fb52ee0a
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 10:07:19 +0100

    Add three different transactions

--- week4_data_structure/FinancialTransactionTracker.java
+++ week4_data_structure/FinancialTransactionTracker.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public class FinancialTransactionTracker {
     public static void main(String[] args) {
         
-        ArrayList<String> history = new ArrayList<>(); 
+        ArrayList<String> history = new ArrayList<>();
+        
+        history.add("Purchase,50.00,2025-01-15");
+        history.add("Sales,100.00,2025-01-16");
+        history.add("Purchase,10.50,2025-01-16");
     }
 }
