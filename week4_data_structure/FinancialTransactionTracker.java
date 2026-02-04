1ff10f468237005450f08323876177f0448d87b6
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-04 10:17:59 +0100

    Loop through the ArrayList

--- week4_data_structure/FinancialTransactionTracker.java
+++ week4_data_structure/FinancialTransactionTracker.java
@@ -9,5 +9,10 @@ public class FinancialTransactionTracker {
         history.add("Purchase,50.00,2025-01-15");
         history.add("Sales,100.00,2025-01-16");
         history.add("Purchase,10.50,2025-01-16");
+
+        System.out.println("--- All Transactions ---");
+        for (String record : history) {
+            System.out.println(record);
+        }
     }
 }
