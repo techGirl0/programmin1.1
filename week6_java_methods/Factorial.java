4c6079fc5bf86e3250b94cd8f6204a33cf5d5123
techGirl0 <254766232+techGirl0@users.noreply.github.com>
2026-02-18 10:04:11 +0100

    implement recursive step (n * factorial(n-1))

--- week6_java_methods/Factorial.java
+++ week6_java_methods/Factorial.java
@@ -7,8 +7,8 @@ public class Factorial {
 
     public static int factorial(int n) {
         if (n == 1) {
-            return 1; 
+            return 1; // base case
         }
-        return 0; 
+        return n * factorial(n - 1); // recursive step
     }
 }
